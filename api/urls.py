a315dfe7ef297aa0de14f41775ee6a0d7e071d20
David <davidcitorojas15@gmail.com>
2024-07-12 08:11:25 -0400

    solicitudes

--- api/urls.py
+++ api/urls.py
@@ -2,11 +2,13 @@ from django.urls import path, include
 from rest_framework import routers
 
 from api.viewsets import LoginViewSet, CamionViewSet, RutaViewSet, ChoferViewSet
+from api.viewsets.solicitud_viewset import SolicitudViewSet
 
 router = routers.DefaultRouter()
 router.register(r'camiones', CamionViewSet)
 router.register(r'rutas', RutaViewSet)
 router.register(r'chofer', ChoferViewSet, basename='chofer')
+router.register(r'solicitudes', SolicitudViewSet)
 router.register('', LoginViewSet, basename='login')
 
 urlpatterns = [
